1ee2413c53ece1b676db10a4065e5b6ac90f75b0
DarkDragon <darkdragon117yt@gmail.com>
2020-07-20 14:46:13 +0200

    Added an update checker

---
+++ src/dev/darkdragon/motdchanger/Updater.java
@@ -0,0 +1,40 @@
+package dev.darkdragon.motdchanger;
+
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.Plugin;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class Updater {
+    motdchanger plugin;
+    String newVersion;
+    URL checkUrl;
+    public Updater(motdchanger motdchanger) {
+        plugin = motdchanger;
+        if(plugin.getConfig().getBoolean("checkupdates")) {
+            try {
+                checkUrl = new URL("https://api.spigotmc.org/legacy/update.php?resource=63607");
+                checkUpdates();
+                if (!newVersion.equals(plugin.getDescription().getVersion())) {
+                    plugin.sendMessage(ChatColor.YELLOW + "A new update is available! Version: "+newVersion);
+                }
+            } catch (IOException e) {
+                plugin.sendMessage(ChatColor.RED + "Couldn't connect to Spigot, no updates available");
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    public void checkUpdates() throws IOException {
+        URLConnection con = checkUrl.openConnection();
+        newVersion = new BufferedReader(new InputStreamReader(con.getInputStream())).readLine();
+    }
+
+}
