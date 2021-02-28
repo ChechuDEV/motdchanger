f40c7aaeaa0b6af7563bf8b526783c8ebe469e96
BuildTools <22799163+DarkDragon117@users.noreply.github.com>
2021-03-01 00:17:02 +0100

    Optimised migration code.

--- src/main/java/dev/tsetsi/motdchanger/Main.java
+++ src/main/java/dev/tsetsi/motdchanger/Main.java
@@ -19,70 +19,71 @@ public final class Main extends JavaPlugin {
         logger = getLogger();
         logger.info("Remember to rate and share this plugin. You can also join my discord server: discord.darkdragon.dev");
 
-        // CONFIGURATION FILES CREATION
-        File configFile = new File(getDataFolder(), "config.yml");
-        if ( !configFile.exists() ) {
-            logger.warning("Config file not found, creating it.");
-            if ( getDataFolder().mkdir() ) logger.info("Plugin folder created");
-            if ( !createConfigFile() ) {
-                logger.severe("Couldn't create the config file, disabling the plugin!");
-                getPluginLoader().disablePlugin(this);
-            }
-        }
+        // CONFIGURATION FILE CREATION
+        saveDefaultConfig();
         // CONFIGURATION MIGRATION
+        boolean migrated = false;
+        if (!getConfig().contains("metrics")) getConfig().set("metrics",true);
+        if (!getConfig().contains("check-updates")) if(getConfig().contains("checkupdates")) {
+            getConfig().set("check-updates",getConfig().get("checkupdates"));
+            getConfig().set("checkupdates",null);
+            migrated = true;
+        } else {
+            getConfig().set("check-updates",true);
+            migrated = true;
+        }
+        if (!getConfig().contains("rotation")) getConfig().set("rotation",true);
         if (getConfig().contains("permanent-motd") && !(getConfig().get("permanent-motd") instanceof List)) {
-            logger.severe("ASDSDASD");
-            String lines[] = getConfig().getString("permanent-motd").split("%newline%",2);
+            String[] lines = getConfig().getString("permanent-motd").split("%newline%",2);
             List<String> permanentMotd = new ArrayList<>(Arrays.asList(lines));
             if (!getConfig().getString("permanent-motd").contains("%newline%")) permanentMotd.add("");
-                getConfig().set("permanent-motd", permanentMotd);
+            getConfig().set("permanent-motd", permanentMotd);
+            migrated = true;
         } else if (!getConfig().contains("permanent-motd")) {
             List<String> permanentMotd = new ArrayList<>(Arrays.asList("§bServer is running smooth...", "&6Be happy!"));
             getConfig().set("permanent-motd", permanentMotd);
+            migrated = true;
         }
         if (!getConfig().contains("rotating-motds")) {
             List<String> rotatingMotd = new ArrayList<>(Arrays.asList("§bServer is rotating smooth...","&6Rotate happily!"));
             List<List<String>> rotatingMotds = new ArrayList<>(Collections.singletonList(rotatingMotd));
             getConfig().set("rotating-motds",rotatingMotds);
+            migrated = true;
         }
 
         File oldMotdFile = new File(getDataFolder(), "motds.yml");
         if (oldMotdFile.exists()) {
+            logger.info("Your configuration file will be updated with your configuration in motds.yml.");
             FileConfiguration motdsFile = YamlConfiguration.loadConfiguration(oldMotdFile);
             if (motdsFile.contains("permanent-motd")) {
-                String lines[] = motdsFile.getString("permanent-motd").split("%newline%",2);
+                String[] lines = motdsFile.getString("permanent-motd").split("%newline%",2);
                 List<String> permanentMotd = new ArrayList<>(Arrays.asList(lines));
                 if (!motdsFile.getString("permanent-motd").contains("%newline%")) permanentMotd.add("");
-
                 getConfig().set("permanent-motd", permanentMotd);
+                migrated = true;
             }
             List<List<String>> rotatingMotds = new ArrayList<>();
             for (int i = 1; i <= 10; i++) {
                 if (motdsFile.contains("motd-rotation"+i) && !motdsFile.getString("motd-rotation" + i).equals("")){
-                    String lines[] = motdsFile.getString("motd-rotation"+i).split("%newline%",2);
+                    String[] lines = motdsFile.getString("motd-rotation"+i).split("%newline%",2);
                     List<String> rotatingMotd = new ArrayList<>(Arrays.asList(lines));
                     if (!motdsFile.getString("motd-rotation"+i).contains("%newline%")) rotatingMotd.add("");
                     rotatingMotds.add(rotatingMotd);
                 }
                 if (i == 10) getConfig().set("rotating-motds", rotatingMotds);
+                migrated = true;
             }
+            File endMotdFile = new File(getDataFolder(), "oldmotds.yml");
+            if(oldMotdFile.renameTo(endMotdFile)) logger.info("The motds.yml file has been renamed to oldmotds.yml.");
         }
-        saveConfig();
+        if(migrated) {
+            saveConfig();
+            logger.info("Your configuration has migrated to a new version, please check that everything is okay. Comments may be deprecated...");
+        }
 
         // COMMAND CREATION
         getCommand("motdchange").setExecutor(new Commands());
     }
-
-    private boolean createConfigFile() {
-        try {
-            Files.copy(Objects.requireNonNull(getResource("config.yml")), new File(getDataFolder(), "config.yml").toPath());
-            return true;
-        } catch (IOException exception) {
-            logger.severe(exception.getMessage());
-        }
-        return false;
-    }
-
 
     @Override
     public void onDisable() {
