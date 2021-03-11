efa6fee64d6367ed65d71a0e163d560d1963f00f
BuildTools <22799163+DarkDragon117@users.noreply.github.com>
2021-03-11 23:38:12 +0100

    Totally restarted PingEvent

--- bukkit/src/main/java/dev/tsetsi/motdchanger/bukkit/PingEvent.java
+++ bukkit/src/main/java/dev/tsetsi/motdchanger/bukkit/PingEvent.java
@@ -13,34 +13,13 @@ import java.util.List;
 import java.util.Objects;
 
 public class PingEvent implements Listener {
-    Plugin plugin;
-    public PingEvent(Main plugin) {
-        this.plugin = plugin;
-    }
-
-    private String getRandomMotd() throws Exception {
-        int n = (int) (Math.random() * (Objects.requireNonNull(plugin.getConfig().getList("rotating-motds")).size()));
-        if ( plugin.getConfig().getList("rotating-motds").get(n) instanceof List) {
-            // (List<String>) plugin.getConfig().getList("rotating-motds").get(n);
-            Gson gson = new Gson();
-            List<String> randomMotd = gson.fromJson(gson.toJson(Objects.requireNonNull(plugin.getConfig().getList("rotating-motds")).get(n)), new TypeToken<List<String>>(){}.getType());
-            if (randomMotd.size() != 2) throw new Exception("There is an error in your config.yml file");
-            String refractoredMotd = randomMotd.get(0) + "\n" + randomMotd.get(1);
-            return refractoredMotd;
-        }
-        else throw new Exception("There is an error in your config.yml file");
+    Motd motd;
+    public PingEvent(Motd motd) {
+        this.motd = motd;
     }
 
     @EventHandler
     public void onServerPing(ServerListPingEvent e) {
-        if (plugin.getConfig().getBoolean("rotation") && Objects.requireNonNull(plugin.getConfig().getList("rotating-motds")).size() != 0) {
-            try {
-                String randomMotd = getRandomMotd();
-                plugin.getLogger().info(randomMotd);
-                e.setMotd(randomMotd);
-            } catch (Exception exception) {
-                exception.printStackTrace();
-            }
-        }
+        e.setMotd(motd.getMotd());
     }
 }
