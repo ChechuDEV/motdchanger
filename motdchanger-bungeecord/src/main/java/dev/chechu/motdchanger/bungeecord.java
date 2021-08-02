400bcfa5bc10b7f5a87a5afe86e45f4e328b2181
DarkDragon117 <22799163+DarkDragon117@users.noreply.github.com>
2021-08-02 14:59:18 +0200

    Initialized project

---
+++ motdchanger-bungeecord/src/main/java/dev/chechu/motdchanger/bungeecord.java
@@ -0,0 +1,16 @@
+package dev.chechu.motdchanger;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public final class bungeecord extends Plugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ motdchanger-paper/src/main/java/dev/chechu/motdchanger/events/bukkitPingListener.java
@@ -0,0 +1,12 @@
+package dev.chechu.motdchanger.events;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.server.ServerListPingEvent;
+
+public class bukkitPingListener implements Listener {
+    @EventHandler
+    private void onServerListPing(ServerListPingEvent event) {
+        // PROTOCOLIB?
+    }
+}
