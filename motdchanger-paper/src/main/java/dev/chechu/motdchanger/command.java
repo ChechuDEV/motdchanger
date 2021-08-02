6e3205d3669e3ef2e9a3b62b7c9aeb17fc78e14f
DarkDragon117 <22799163+DarkDragon117@users.noreply.github.com>
2021-08-02 14:59:39 +0200

    Initialized project, added command class

---
+++ motdchanger-paper/src/main/java/dev/chechu/motdchanger/command.java
@@ -0,0 +1,15 @@
+package dev.chechu.motdchanger;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+
+public class command implements CommandExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        return false;
+
+    }
+
+}
