4b3ed52086d1868598b40618a6a08f16279f8454
Chechu <22799163+DarkDragon117@users.noreply.github.com>
2022-01-30 00:04:37 +0100

    Renamed from paper.java to Main.java Added the new Configuration system and more descriptive messages when loading the event hooks.

--- motdchanger-paper/src/main/java/dev/chechu/motdchanger/Main.java
+++ motdchanger-paper/src/main/java/dev/chechu/motdchanger/Main.java
@@ -1,5 +1,6 @@
 package dev.chechu.motdchanger;
 
+import dev.chechu.motdchanger.commands.MainCommand;
 import dev.chechu.motdchanger.events.bukkitPingListener;
 import dev.chechu.motdchanger.events.packetPingListener;
 import dev.chechu.motdchanger.events.paperPingListener;
@@ -9,13 +10,17 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.io.File;
 import java.util.logging.Logger;
 
-public final class paper extends JavaPlugin {
+public final class Main extends JavaPlugin {
     private Logger log;
     public String motD;
+
+    private Configuration config = null;
     @Override
     public void onEnable() {
         log = getLogger();
         log.info("Thanks for using my plugin! Remember to rate it and share it with your friends!");
+
+        config = new Configuration(this);
 
         // Event hook
         eventHook();
@@ -23,20 +28,20 @@ public final class paper extends JavaPlugin {
         // Command init
         PluginCommand motDChange = getCommand("motdchange");
         assert motDChange != null;
-        motDChange.setExecutor(new command(this));
+        motDChange.setExecutor(new MainCommand(config));
 
         // Config file set up
         File configFile = new File(getDataFolder(),"config.yml");
         if (!configFile.exists() || !getConfig().contains("version")) {
             saveDefaultConfig();
-        } // CONFIG FILE VERSION IS USELESS FOR NOW
+        } // TODO: CONFIG FILE VERSION IS USELESS FOR NOW
 
         // Metrics
         new Metrics(this, 4679);
 
 
         motD = getConfig().getStringList("motds").get(0);
-        // Autoupdate
+        // TODO: Autoupdate
     }
 
     private boolean isPaper() {
@@ -58,11 +63,18 @@ public final class paper extends JavaPlugin {
     }
 
     private void eventHook() {
-        if(isPaper()) getServer().getPluginManager().registerEvents(new paperPingListener(this),this);
-        else if (hasProtocol()) new packetPingListener(this);
+        if(isPaper()) {
+            getServer().getPluginManager().registerEvents(new paperPingListener(config), this);
+            log.info("Initializing Paper Ping Listener");
+        }
+        else if (hasProtocol()) {
+            new packetPingListener(config);
+            log.info("Initializing ProtocolLib Ping Packet Listener");
+        }
         else {
-            getServer().getPluginManager().registerEvents(new bukkitPingListener(this),this);
-            log.warning("It seems that you aren't using Paper nor ProtocolLib, this plugin will be limited but will still work.");
+            getServer().getPluginManager().registerEvents(new bukkitPingListener(config),this);
+            log.warning("It seems that you aren't using neither Paper nor ProtocolLib, this plugin will be limited but will still work.");
+            log.info("Initializing Bukkit Ping Listener");
         }
     }
 
