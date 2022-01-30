e79a775b471d34a386dd3aeaaf1eadd833a86c94
Chechu <22799163+DarkDragon117@users.noreply.github.com>
2022-01-30 15:37:38 +0100

    Added info message template. Added click event parameters.

--- motdchanger-paper/src/main/java/dev/chechu/motdchanger/utils/Message.java
+++ motdchanger-paper/src/main/java/dev/chechu/motdchanger/utils/Message.java
@@ -1,8 +1,11 @@
 package dev.chechu.motdchanger.utils;
 
+import dev.chechu.motdchanger.Main;
+import net.md_5.bungee.api.chat.ClickEvent;
 import org.bukkit.ChatColor;
 
 public enum Message {
+    INFO(Level.INFO, "This plugin has been made by Chechu (at chechu.dev).", ChatColor.AQUA + "Version " + Main.getVersion(), "https://chechu.dev/", ClickEvent.Action.OPEN_URL),
     PERMISSION(Level.ERROR, "You don't have enough permissions to execute this command.", ChatColor.RED + "Required permission: " + ChatColor.GRAY + "%s"),
     PERMANENT_SUCCESS(Level.SUCCESS, "Permanent Message of the Day successfully set.", "%s"),
     TEMPORARY_SUCCESS(Level.SUCCESS, "Temporary Message of the Day successfully set.", "%s"),
@@ -11,6 +14,8 @@ public enum Message {
     final Level level;
     final String message;
     String hover;
+    String click;
+    ClickEvent.Action clickAction;
 
     Message(Level level, String message) {
         this.level = level;
@@ -21,5 +26,13 @@ public enum Message {
         this.level = level;
         this.message = message;
         this.hover = hover;
+    }
+
+    Message(Level level, String message, String hover, String click, ClickEvent.Action clickAction) {
+        this.level = level;
+        this.message = message;
+        this.hover = hover;
+        this.click = click;
+        this.clickAction = clickAction;
     }
 }
