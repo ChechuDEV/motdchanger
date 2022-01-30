effb46f6b08e258397c463ac9125f393cf41c147
Chechu <22799163+DarkDragon117@users.noreply.github.com>
2022-01-30 15:37:09 +0100

    Finished message converter and sender. Made the functions static.

--- motdchanger-paper/src/main/java/dev/chechu/motdchanger/utils/Messages.java
+++ motdchanger-paper/src/main/java/dev/chechu/motdchanger/utils/Messages.java
@@ -1,30 +1,38 @@
 package dev.chechu.motdchanger.utils;
 
-import net.md_5.bungee.api.chat.BaseComponent;
-import net.md_5.bungee.api.chat.ComponentBuilder;
-import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.*;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.chat.hover.content.Content;
 import net.md_5.bungee.api.chat.hover.content.Text;
 import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import java.awt.*;
 
 public class Messages {
-    public void sendMessage(Player player, String string) {
-        player.sendMessage(ChatColor.GREEN + "MOTDChanger » " + string);
-    }
-    public void sendMessage(Player player, String string, boolean inline) {
-        if (inline) player.sendMessage(string);
-        else sendMessage(player, string);
+    public static void sendMessage(CommandSender receiver, String string) {
+        receiver.sendMessage(ChatColor.GREEN + "MOTDChanger » " + string);
     }
 
-    public void sendMessage(Player player, Message message) {
+    public static void sendMessage(CommandSender receiver, String string, boolean inline) {
+        if (inline) receiver.sendMessage(string);
+        else sendMessage(receiver, string);
+    }
+
+    public static void sendMessage(CommandSender receiver, Message message) {
         BaseComponent messageText = new TextComponent(message.level.color + "MotDChanger » " + message.message);
+        HoverEvent hoverEvent = null;
         if(message.hover != null) {
             Text contents = new Text(message.hover);
-            HoverEvent hoverEvent = new HoverEvent(HoverEvent.Action.SHOW_TEXT, contents);
+            hoverEvent = new HoverEvent(HoverEvent.Action.SHOW_TEXT, contents);
         }
+        ClickEvent clickEvent = null;
+        if(message.click != null) {
+            clickEvent = new ClickEvent(message.clickAction, message.click);
+        }
+        if (hoverEvent != null) messageText.setHoverEvent(hoverEvent);
+        if (clickEvent != null) messageText.setClickEvent(clickEvent);
+        receiver.sendMessage(messageText);
     }
 }
