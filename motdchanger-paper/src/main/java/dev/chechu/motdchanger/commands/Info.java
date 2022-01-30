16868f77b36dcf70d943880be79ae2f3533744cf
Chechu <22799163+DarkDragon117@users.noreply.github.com>
2022-01-30 15:05:55 +0100

    Created info command class.

--- motdchanger-paper/src/main/java/dev/chechu/motdchanger/commands/Info.java
+++ motdchanger-paper/src/main/java/dev/chechu/motdchanger/commands/Info.java
@@ -1,4 +1,17 @@
 package dev.chechu.motdchanger.commands;
 
-public class Info {
+import org.bukkit.command.CommandSender;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Info extends Command {
+    public Info() {
+        super("Displays information about this plugin", Collections.emptyList(), "info", Collections.emptyList());
+    }
+
+    @Override
+    public void execute(CommandSender sender, String[] args) {
+
+    }
 }
