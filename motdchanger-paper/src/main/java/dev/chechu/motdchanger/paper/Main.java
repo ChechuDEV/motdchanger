09e1358ad1a92cb782181c467ce3ad5a68f96bc4
Chechu <22799163+DarkDragon117@users.noreply.github.com>
2022-03-13 01:40:45 +0100

    Added UPDATER!!

--- motdchanger-paper/src/main/java/dev/chechu/motdchanger/paper/Main.java
+++ motdchanger-paper/src/main/java/dev/chechu/motdchanger/paper/Main.java
@@ -1,9 +1,13 @@
 package dev.chechu.motdchanger.paper;
 
+import dev.chechu.dragonapi.core.Core;
+import dev.chechu.dragonapi.core.Updater;
+import dev.chechu.dragonapi.spigot.SpigotUpdater;
 import dev.chechu.motdchanger.paper.commands.MainCommand;
 import dev.chechu.motdchanger.paper.events.bukkitPingListener;
 import dev.chechu.motdchanger.paper.events.packetPingListener;
 import dev.chechu.motdchanger.paper.events.paperPingListener;
+import org.bukkit.Bukkit;
 import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -11,16 +15,18 @@ import java.io.File;
 import java.util.logging.Logger;
 
 public final class Main extends JavaPlugin {
-    private Logger log;
     public String motD;
 
     private Configuration config = null;
     @Override
     public void onEnable() {
-        log = getLogger();
-        log.info("Thanks for using my plugin! Remember to rate it and share it with your friends!");
-
+        new Core(false);
         config = new Configuration(this);
+        config.getLogger().info("Thanks for using my plugin! Remember to rate it and share it with your friends!");
+
+        // Dragon-API updater
+        Updater updater = new SpigotUpdater(this, "", config.getAutoUpdate().getValue());
+        Bukkit.getScheduler().runTaskTimer(this, () -> updater.tryUpdate(false), 0, 36000L);
 
         // Event hook
         eventHook();
@@ -30,19 +36,12 @@ public final class Main extends JavaPlugin {
         assert motDChange != null;
         motDChange.setExecutor(new MainCommand(config));
 
-        // Config file set up
-        File configFile = new File(getDataFolder(),"config.yml");
-        if (!configFile.exists() || !getConfig().contains("version")) {
-            saveDefaultConfig();
-        } // TODO: CONFIG FILE VERSION IS USELESS FOR NOW
-
         // Metrics
-        if(config.isMetrics())
+        if(config.getMetrics().getValue())
             new Metrics(this, 4679);
 
 
         motD = config.getMotD();
-        // TODO: Autoupdate
     }
 
     private boolean isPaper() {
@@ -66,16 +65,16 @@ public final class Main extends JavaPlugin {
     private void eventHook() {
         if(isPaper()) {
             getServer().getPluginManager().registerEvents(new paperPingListener(config), this);
-            log.info("Initializing Paper Ping Listener");
+            config.getLogger().info("Initializing Paper Ping Listener");
         }
         else if (hasProtocol()) {
             new packetPingListener(config);
-            log.info("Initializing ProtocolLib Ping Packet Listener");
+            config.getLogger().info("Initializing ProtocolLib Ping Packet Listener");
         }
         else {
             getServer().getPluginManager().registerEvents(new bukkitPingListener(config),this);
-            log.warning("It seems that you aren't using neither Paper nor ProtocolLib, this plugin will be limited but will still work.");
-            log.info("Initializing Bukkit Ping Listener");
+            config.getLogger().warning("It seems that you aren't using neither Paper nor ProtocolLib, this plugin will be limited but will still work.");
+            config.getLogger().info("Initializing Bukkit Ping Listener");
         }
     }
 
