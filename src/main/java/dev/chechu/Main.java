a39728892ced38b83469df418be23efd5edd98ab
Chechu <22799163+DarkDragon117@users.noreply.github.com>
2022-09-03 21:09:34 +0200

    Initial commit

---
+++ src/main/java/dev/chechu/Main.java
@@ -0,0 +1,10 @@
+package dev.chechu;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        // TODO
+    }
+}
---
+++ src/test/java/dev/chechu/AppTest.java
@@ -0,0 +1,14 @@
+package dev.chechu;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest 
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
