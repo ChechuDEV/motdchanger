2a27c05e45822d6ce4aa9ac323259c8a38c209a1
Chechu <22799163+DarkDragon117@users.noreply.github.com>
2022-09-05 11:01:23 +0200

    ✨ Created a simple MOTDManager

---
+++ src/main/java/dev/chechu/motdchanger/MOTDManager.java
@@ -0,0 +1,107 @@
+package dev.chechu.motdchanger;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.function.Consumer;
+
+import org.bukkit.ChatColor;
+
+import dev.chechu.motdchanger.exceptions.EmptyListException;
+import net.kyori.adventure.platform.bukkit.BukkitComponentSerializer;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.text.minimessage.ParsingException;
+
+public class MOTDManager {
+    private final List<Component> mOTDList = new ArrayList<>();
+
+    private final MiniMessage miniMessage = MiniMessage.miniMessage();
+    private final Consumer<MOTDManager> saveAction;
+
+    private boolean rotation;
+
+    public MOTDManager(Consumer<MOTDManager> saveAction) {
+        this.saveAction = saveAction;
+    }
+
+    public void reload(List<String> mOTDStrings, boolean rotation) throws EmptyListException {
+        mOTDList.clear();
+
+        if (mOTDStrings.isEmpty()) {
+            throw new EmptyListException();
+        }
+
+        for (String string : mOTDStrings) {
+            mOTDList.add(convert(string));
+        }
+
+        this.rotation = rotation;
+    }
+
+    private String translateColorSymbols(String string) {
+        return ChatColor.translateAlternateColorCodes('&', string);
+    }
+
+    private Component deserialize(String content) {
+        try {
+            return miniMessage.deserialize(content);
+        } catch (ParsingException e) {
+            return BukkitComponentSerializer.legacy().deserialize(content);
+        }
+    }
+
+    private Component convert(String content) {
+        return deserialize(translateColorSymbols(content));
+    }
+
+    public String legacySerialize(Component component) {
+        return BukkitComponentSerializer.legacy().serialize(component);
+    }
+
+    public void addMOTD(String mOTD) {
+        mOTDList.add(convert(mOTD));
+        save();
+    }
+
+    public void removeMOTD(int index) {
+        mOTDList.remove(index);
+        save();
+    }
+
+    public void setMOTD(int index, String mOTD) {
+        mOTDList.set(index, convert(mOTD));
+    }
+
+    private void save() {
+        saveAction.accept(this);
+    }
+
+    public List<Component> getAllMOTD() {
+        return mOTDList;
+    }
+
+    public List<String> serializeAllMOTD() {
+        List<String> list = new ArrayList<>();
+        for (Component component : getAllMOTD()) {
+            list.add(miniMessage.serialize(component));
+        }
+        return list;
+    }
+
+    public boolean isRotation() {
+        return rotation;
+    }
+
+    private final Random random = new Random();
+
+    public Component getMOTD() {
+        if (mOTDList.isEmpty()) {
+            return convert("<cyan>Server is running smooth :)</cyan><newline><gold>Be happy!</gold>");
+        }
+        if (isRotation()) {
+            return mOTDList.get(random.nextInt(mOTDList.size()));
+        }
+        return mOTDList.get(0);
+    }
+}
