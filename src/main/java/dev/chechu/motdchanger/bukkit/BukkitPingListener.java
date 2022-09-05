2554242aee24ab387de2efe6b144d84a171d100e
Chechu <22799163+DarkDragon117@users.noreply.github.com>
2022-09-05 11:01:46 +0200

    🚧 Added a Bukkit Ping Listener

---
+++ src/main/java/dev/chechu/motdchanger/bukkit/BukkitPingListener.java
@@ -0,0 +1,11 @@
+package dev.chechu.motdchanger.bukkit;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.server.ServerListPingEvent;
+
+public class BukkitPingListener implements Listener {
+    @EventHandler
+    public void onPing(ServerListPingEvent event) {
+    }
+}
