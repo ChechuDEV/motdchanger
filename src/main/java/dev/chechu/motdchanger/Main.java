e486b97475d51ea565113683ca4900f2c982079c
Chechu <22799163+DarkDragon117@users.noreply.github.com>
2022-09-05 11:01:03 +0200

    ✨ Added on enable logic

--- src/main/java/dev/chechu/motdchanger/Main.java
+++ src/main/java/dev/chechu/motdchanger/Main.java
@@ -1,26 +1,56 @@
 package dev.chechu.motdchanger;
 
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import dev.chechu.dragonapi.spigot.SpigotUpdater;
+import dev.chechu.motdchanger.bukkit.BukkitPingListener;
+
 public class Main extends JavaPlugin {
+    private static MOTDManager manager;
+
+    public static MOTDManager getManager() {
+        return manager;
+    }
+
     @Override
     public void onEnable() {
+        new SpigotUpdater(this, "63607", false);
+
+        manager = new MOTDManager((manager) -> {
+            getConfig().set("motds", manager.serializeAllMOTD());
+            getConfig().set("rotation", manager.isRotation());
+            saveConfig();
+        });
+        if (isPaper()) {
+            initPaper();
+        } else {
+            initBukkit();
+        }
     }
 
-    public void initPaper() {
-
+    private void registerListener(Listener listener) {
+        getServer().getPluginManager().registerEvents(listener, this);
     }
 
-    public void initBukkit() {
-
+    private void initPaper() {
+        initBukkit();
     }
 
-    public boolean isPaper() {
+    private void initBukkit() {
+        registerListener(new BukkitPingListener());
+    }
+
+    private boolean isPaper() {
         try {
             Class.forName("com.destroystokyo.paper.ParticleBuilder");
             return true;
         } catch (ClassNotFoundException e) {
             return false;
         }
+    }
+
+    @Override
+    public void onDisable() {
     }
 }
