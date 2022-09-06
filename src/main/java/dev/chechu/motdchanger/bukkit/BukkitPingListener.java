20e627c74d451b62e8481703596622633fa776f0
Chechu <22799163+DarkDragon117@users.noreply.github.com>
2022-09-06 12:39:18 +0200

    ✨ Finished on ping for both paper and bukkit

--- src/main/java/dev/chechu/motdchanger/bukkit/BukkitPingListener.java
+++ src/main/java/dev/chechu/motdchanger/bukkit/BukkitPingListener.java
@@ -4,8 +4,16 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.server.ServerListPingEvent;
 
+import dev.chechu.motdchanger.MOTDManager;
+import dev.chechu.motdchanger.Main;
+import net.kyori.adventure.text.Component;
+
 public class BukkitPingListener implements Listener {
     @EventHandler
     public void onPing(ServerListPingEvent event) {
+        MOTDManager manager = Main.getMOTDManager();
+        Component component = manager.getMOTD();
+
+        event.setMotd(manager.legacySerialize(component));
     }
 }
---
+++ src/main/java/dev/chechu/motdchanger/paper/PaperPingListener.java
@@ -0,0 +1,15 @@
+package dev.chechu.motdchanger.paper;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+import com.destroystokyo.paper.event.server.PaperServerListPingEvent;
+
+import dev.chechu.motdchanger.Main;
+
+public class PaperPingListener implements Listener {
+    @EventHandler
+    public void onPing(PaperServerListPingEvent event) {
+        event.motd(Main.getMOTDManager().getMOTD());
+    }
+}
